086baba7dc40ea0a99fcc8f7bb9af792b5cc99cb
MNTMDEV <mntmdev@163.com>
2022-08-07 00:42:49 +0800

    fix:service路径问题

--- setup.py
+++ setup.py
@@ -3,15 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name="certbotool",
     version="1.0.0",
-    keywords=[],
-    description="certbot toolkit",
-    long_description="certbot toolkit",
-    license="GPL Licence",
-    python_requires='>=3.6',
-
-    url="https://www.mntmdev.com",
-    author="mntmdev",
-    author_email="admin@mntmdev.com",
 
     packages=find_packages(),
     include_package_data=True,
@@ -19,9 +10,9 @@ setup(
     install_requires=[
         'aliyun_python_sdk_alidns',
         'aliyun_python_sdk_core',
-        'APScheduler',
+        'apscheduler',
         'requests',
-        'setuptools'
+        'setuptools',
     ],
     
     scripts=[],
